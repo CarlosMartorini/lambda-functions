15ece5bc20bbe0299a79e605652048be6a35526c
Carlos Martorini <c7.silveira@gmail.com>
2021-09-07 07:36:06 -0300

    Created the functions with lambda

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# Escreva seu código aqui
+from functools import reduce
+
+
+def characters_filter(list_of_characters, key, value):
+    return list(filter(lambda item: item[key] == value, list_of_characters))
+
+
+def team_power(list_of_characters):
+    return sum(map(lambda item: item["power"], list_of_characters), 0)
+
+
+def team_power_2(list_of_characters, advantage):
+    return reduce((lambda total,item: total + item["power"]), list_of_characters, advantage)
